9c5cde65ce919d9fc1df92ce6f1442346255b30c
davidtheblane <davidtheblane@gmail.com>
2019-10-11 05:12:47 -0300

    Imagens Funcionando

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import api from '../../services/api';
 
-export default function Dashboard(){
-  return <h1>Dashboard</h1>
+
+
+export default function Dashboard() {
+  const [spots, setSpots] = useState([]);
+  
+  useEffect(() => {
+    async function loadSpots() {
+      const user_id = localStorage.getItem('user')
+      const response = await api.get('/dashboard', {
+        headers: { user_id}
+      });
+
+      setSpots(response.data); 
+    }
+
+      loadSpots();
+  }, []);
+
+
+  return (
+  <>
+  <ul className="spot-list">
+    {spots.map(spot => (
+      <li key={spot._id}>
+        <header style={{backgroundImage: `url(${spot.thumbnail.url})`}}/>
+        <strong>{spot.company}</strong>
+        <span>{spot.price}</span>
+      </li>
+
+    ))}
+  </ul>
+  </>
+    )
 }
