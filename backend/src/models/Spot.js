076c7185e4829798f4813e2bd75506ab74b3b228
davidtheblane <davidtheblane@gmail.com>
2019-10-11 05:19:43 -0300

    atualizando tudo agora

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -1,14 +1,25 @@
 const mongoose = require("mongoose");
 
-const SpotSchema = new mongoose.Schema({
-  thumbnail: String,
-  company: String,
-  price: Number,
-  techs: [String], //pra salvar em array, usar dentro de colchetes
-  user: {
-    type: mongoose.Schema.Types.ObjectId, //ref ID do usuario como objeto
-    ref: "User" //qual model é essa informação
+const SpotSchema = new mongoose.Schema(
+  {
+    thumbnail: String,
+    company: String,
+    price: Number,
+    techs: [String], //pra salvar em array, usar dentro de colchetes
+    user: {
+      type: mongoose.Schema.Types.ObjectId, //ref ID do usuario como objeto
+      ref: "User" //qual model é essa informação
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
   }
+);
+
+SpotSchema.virtual("thumbnail_url").get(function() {
+  return `http://localhost:3333/files/${this.thumbnail}`;
 });
 
 module.exports = mongoose.model("Spot", SpotSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,28 @@
-const express = require ('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omni:omni@omnistackdavi-xj47e.mongodb.net/semana09?retryWrites=true&w=majority', {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-})
-
-
+mongoose.connect(
+  "mongodb+srv://omni:omni@omnistackdavi-xj47e.mongodb.net/semana09?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 //GET, POST, PUT, DELETE
 // req.query = Acessar query params (para filtros)
 //req.params = Acessar route params (para edição e delete)
 //req.body = Acessar o corpo do requisição (para criação, edição)
 
-
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
